1d33ebe63cc510c9ca0d96a4977bbd0bb5a3ac0d
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-10-16 12:52:18 +0100

    Fix HTML entity in congratulation message

--- app/(components)/congratulation/page.tsx
+++ app/(components)/congratulation/page.tsx
@@ -26,7 +26,7 @@ export default function Congratulations(){
             <div className="congrats bg-green-500 flex rounded-full w-30 h-30 items-center justify-center">
                 <Check strokeWidth={5} className="text-white w-20 h-20"/>
             </div>
-            <p className="text-lg font-bolds">Congratulations, you've successfully create a Neon Bank Account</p>
+            <p className="text-lg font-bolds">Congratulations, you&apos;ve successfully create a Neon Bank Account</p>
             <p className="text-sm font-regular">Please check your email for email confirmation</p>
       </div>
     )

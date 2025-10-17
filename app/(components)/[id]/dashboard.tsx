0613ad4d25b2d826e09853cd3b7de83ecfadcecb
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-10-17 13:38:47 +0100

    Adjust layout and styles in dashboard component

--- app/(components)/[id]/dashboard.tsx
+++ app/(components)/[id]/dashboard.tsx
@@ -24,7 +24,7 @@ export default function Dashboard({handleSavings, handleTransfer, bal,handleLoan
             <button onClick={handleSavings} className="max-[582]:truncate rounded-lg max-[496]:justify-between max-[496]:max-w-[250px] max-[717]:text-lg max-[666]:text-sm hover:bg-primary hover:text-white font-bolds max-[918]:text-xl max-[582]:text-[.6rem] max-[582]:font-regular text-2xl flex-1/3 flex items-center justify-between max-sm:px-2 max-sm:py-1 px-4 py-2 bg-white shadow-lg"><PiggyBank className="max-[500]:h-2 max-[500]:w-2 text-white rounded-sm p-1 w-[28px] bg-secondary h-[28]"/><p className="max-[496]:text-sm">Add to savings</p></button>
             <button onClick={handleLoan} className=" max-[582]:truncate rounded-lg max-[496]:justify-between max-[666]:text-sm max-[496]:max-w-[250px] max-[717]:text-lg hover:bg-primary hover:text-white font-bolds max-[918]:text-xl max-[582]:text-[.6rem] max-[582]:font-regular text-2xl flex-1/3 flex items-center justify-between max-sm:px-2 max-sm:py-1 px-4 py-2 bg-white shadow-lg"><Banknote className="text-white max-[500]:h-2 rounded-sm p-1 w-[28px] bg-secondary h-[28] max-[500]:w-2"/><p className="max-[496]:text-sm">Apply for loan</p></button>
         </div>
-        <div className="w-full flex mt-5 h-70 gap-x-4 max-sm:flex-col max-sm:gap-y-2">
+        <div className="w-full flex mt-5 h-70 max-sm:h-fit gap-x-4 max-sm:flex-col max-sm:gap-y-2">
             <div className="flex-2/3 p-4 bg-white rounded-lg shadow-lg h-full">
             <p className="font-regular text-sm text-center fixed">Recent transactions</p>
             {

6953b1e4b8294ddf624997c1c58ca6f60c27dfe0
Rokeeb Maruf <dayorokeeb234@gmail.com>
2025-10-19 13:32:19 +0100

    Update page.tsx

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -167,123 +167,125 @@ export default function Complete() {
         <h2 className="font-boldser text-3xl mb-2">Neon Bank</h2>
         <p className="font-light text-lg">Fortified security, seamless banking</p>
       </div>
-      <form className="right bg-light flex-50 min-h-screen flex flex-col justify-center items-center relative">
-        {
-          display[0] ?(
-          <div className="overLay absolute top-0 left-0 bg-white w-full h-full h-90 p-6 rounded-lg flex flex-col justify-center items-center">
-          {display[1] ?(
-            <X strokeWidth={2} onClick={()=>setDisp(['','','','',''])} className="absolute cursor-pointer top-4 right-4"></X>
-          ):null}
-          {display[2] ?(
-           <div className="wrong w-full">
-            <div className="congrats bg-red-500 mx-auto flex rounded-full w-30 h-30 items-center justify-center">
-                <X strokeWidth={5} className="text-white w-20 h-20"/>
+      <form className="right bg-light flex-50 min-h-screen flex justify-center items-center">
+        <div className="relative flex flex-col justify-center items-center">
+            {
+              display[0] ?(
+              <div className="overLay absolute top-0 left-0 bg-white w-full h-full p-6 rounded-lg flex flex-col justify-center items-center">
+              {display[1] ?(
+                <X strokeWidth={2} onClick={()=>setDisp(['','','','',''])} className="absolute cursor-pointer top-4 right-4"></X>
+              ):null}
+              {display[2] ?(
+               <div className="wrong w-full">
+                <div className="congrats bg-red-500 mx-auto flex rounded-full w-30 h-30 items-center justify-center">
+                    <X strokeWidth={5} className="text-white w-20 h-20"/>
+                </div>
+                <p className=" font-regular text-sm text-center mt-5">Account creation was unsuccessful</p>
+              </div>
+              ):null}
+              {display[3] ?(
+               <div className="wrong w-full">
+                <div className="congrats bg-green-500 mx-auto flex rounded-full w-30 h-30 items-center justify-center">
+                    <Check strokeWidth={5} className="text-white w-20 h-20"/>
+                </div>
+                <p className=" font-regular text-sm text-center mt-5">Log in successfully, pls wait while we redirect you to your dashboard</p>
+              </div>
+              ):null}
+              {display[4]?(
+                <div className="flex items-center justify-between h-16">
+                  <div className="w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-roll"></div>
+                </div>
+              ): null }
             </div>
-            <p className=" font-regular text-sm text-center mt-5">Account creation was unsuccessful</p>
-          </div>
-          ):null}
-          {display[3] ?(
-           <div className="wrong w-full">
-            <div className="congrats bg-green-500 mx-auto flex rounded-full w-30 h-30 items-center justify-center">
-                <Check strokeWidth={5} className="text-white w-20 h-20"/>
+              ) : null
+            }
+            <div className="logo gap-x-2.5 mx-auto items-center justify-center mb-5 hidden max-[800]:flex">
+              <img src="./Images/Neon-dark.png" className="w-10 h-10" alt="" />
+              <h3 className="font-boldser text-4xl">Neon Bank</h3>
             </div>
-            <p className=" font-regular text-sm text-center mt-5">Log in successfully, pls wait while we redirect you to your dashboard</p>
-          </div>
-          ):null}
-          {display[4]?(
-            <div className="flex items-center justify-between h-16">
-              <div className="w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-roll"></div>
+            <h2 className="font-boldser text-3xl text-primary mt-3 mb-4">Finish setting up</h2>
+            <div className={`personal ${disp[0]}`}>
+              <div className="each-part mb-2">
+                <p className="text-primary font-regular text-lg mb-2">Fullname</p>
+                <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = e.target.value
+                  const prevData = {...data}
+                  prevData.Fullname = val
+                  setData(prevData)
+                }} placeholder="John Doe" className="border-primary placeholder:text-[#D5C3C3] outline-0 rounded-lg h-8 w-78 px-3 py-2 border-2" />
+              </div>
+              <div className="each-part mb-2">
+                <p className="text-primary font-regular text-lg mb-2">Nickname</p>
+                <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = e.target.value
+                  const prevData = {...data}
+                  prevData.Nickname = val
+                  setData(prevData)
+                }} placeholder="Johnny" className="border-primary placeholder:text-[#D5C3C3] outline-0 rounded-lg h-8 w-78 px-3 py-2 border-2" />
+              </div>
+              <div className="each-part mt-2">
+                <p className="text-primary font-regular text-lg mb-2">Address</p>
+                <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = e.target.value
+                  const prevData = {...data}
+                  prevData.Address = val
+                  setData(prevData)}} placeholder="Under G, Ogomoso,Nigeria" className="border-primary placeholder:text-[#D5C3C3] h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
+              </div>
+              <div className="each-part mt-2">
+                <p className="text-primary font-regular text-lg mb-2">BVN</p>
+                <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = parseInt(e.target.value)
+                  const prevData = {...data}
+                  prevData.BVN = val.toString()
+                  setData(prevData)}} maxLength={11} pattern='[0-9]+' placeholder="12345678901" className="border-primary placeholder:text-[#D5C3C3] h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
+              </div>
+              <div className="each-part mt-2">
+                <p className="text-primary font-regular text-lg mb-2">Phone number</p>
+                <div className="relative">
+                  <p className="absolute font-regular my-auto pt-1.5 p-1 text-white pr-2 rounded-l-lg bg-primary h-8">+234</p>
+                  <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                    const val = parseInt(e.target.value)
+                    const prevData = {...data}
+                    prevData.phone = `+234${val}`
+                    setData(prevData)}} maxLength={10} pattern='[0-9]+' className="border-primary placeholder:text-[#D5C3C3] pl-11 h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
+                </div>
+              </div>
+              <div className="each-part mt-1 flex items-center justify-between w-78">
+                <p className="text-primary font-regular text-lg mb-2">Date of Birth</p>
+                <input type="date"  onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = e.target.value
+                  const prevData = {...data}
+                  prevData.Date = val
+                  setData(prevData)}} className="text-secondary font-regular text-lg mb-2" />
+              </div>
+              
+              <button className='mt-1 bg-secondary ml-[80%] transition duration-300 hover:bg-primary hover:text-blue-300 text-white px-4 py-1.5 rounded-sm cursor-pointer font-bolds ' onClick={handleFirst}>Next</button>
             </div>
-          ): null }
-        </div>
-          ) : null
-        }
-        <div className="logo gap-x-2.5 mx-auto items-center justify-center mb-5 hidden max-[800]:flex">
-          <img src="./Images/Neon-dark.png" className="w-10 h-10" alt="" />
-          <h3 className="font-boldser text-4xl">Neon Bank</h3>
-        </div>
-        <h2 className="font-boldser text-3xl text-primary mt-3 mb-4">Finish setting up</h2>
-        <div className={`personal ${disp[0]}`}>
-          <div className="each-part mb-2">
-            <p className="text-primary font-regular text-lg mb-2">Fullname</p>
-            <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = e.target.value
-              const prevData = {...data}
-              prevData.Fullname = val
-              setData(prevData)
-            }} placeholder="John Doe" className="border-primary placeholder:text-[#D5C3C3] outline-0 rounded-lg h-8 w-78 px-3 py-2 border-2" />
-          </div>
-          <div className="each-part mb-2">
-            <p className="text-primary font-regular text-lg mb-2">Nickname</p>
-            <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = e.target.value
-              const prevData = {...data}
-              prevData.Nickname = val
-              setData(prevData)
-            }} placeholder="Johnny" className="border-primary placeholder:text-[#D5C3C3] outline-0 rounded-lg h-8 w-78 px-3 py-2 border-2" />
-          </div>
-          <div className="each-part mt-2">
-            <p className="text-primary font-regular text-lg mb-2">Address</p>
-            <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = e.target.value
-              const prevData = {...data}
-              prevData.Address = val
-              setData(prevData)}} placeholder="Under G, Ogomoso,Nigeria" className="border-primary placeholder:text-[#D5C3C3] h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
-          </div>
-          <div className="each-part mt-2">
-            <p className="text-primary font-regular text-lg mb-2">BVN</p>
-            <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = parseInt(e.target.value)
-              const prevData = {...data}
-              prevData.BVN = val.toString()
-              setData(prevData)}} maxLength={11} pattern='[0-9]+' placeholder="12345678901" className="border-primary placeholder:text-[#D5C3C3] h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
-          </div>
-          <div className="each-part mt-2">
-            <p className="text-primary font-regular text-lg mb-2">Phone number</p>
-            <div className="relative">
-              <p className="absolute font-regular my-auto pt-1.5 p-1 text-white pr-2 rounded-l-lg bg-primary h-8">+234</p>
-              <input type="text" onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-                const val = parseInt(e.target.value)
-                const prevData = {...data}
-                prevData.phone = `+234${val}`
-                setData(prevData)}} maxLength={10} pattern='[0-9]+' className="border-primary placeholder:text-[#D5C3C3] pl-11 h-8 outline-0 rounded-lg w-78 px-3 py-2 border-2" />
+            <div className={`submit ${disp[1]}`}>
+                  <Image src={img} width={124} className='mx-auto rounded-full max-w-31 max-h-31' height={124} alt='Profile'></Image>
+                <div className="each-part mt-1 flex items-center justify-between w-78">
+                <p className="text-primary font-regular text-sm">Upload profile picture</p>
+                <input type="file" onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
+                  const val = e.target.files?.[0]
+                  if(val){
+                    const prevData = {...data}
+                    prevData.Profile = val
+                    setData(prevData)
+                    const url = URL.createObjectURL(val)
+                    setImg(url)
+                  }
+                }} accept='image/*' className="text-secondary font-regular w-[160px] text-sm" />
             </div>
-          </div>
-          <div className="each-part mt-1 flex items-center justify-between w-78">
-            <p className="text-primary font-regular text-lg mb-2">Date of Birth</p>
-            <input type="date"  onInput={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = e.target.value
-              const prevData = {...data}
-              prevData.Date = val
-              setData(prevData)}} className="text-secondary font-regular text-lg mb-2" />
-          </div>
-          
-          <button className='mt-1 bg-secondary ml-[80%] transition duration-300 hover:bg-primary hover:text-blue-300 text-white px-4 py-1.5 rounded-sm cursor-pointer font-bolds ' onClick={handleFirst}>Next</button>
+            <div className="each-part mt-1 flex items-center justify-start w-78">
+              <input type="checkbox" onClick={()=>setCheck(!check)} className="font-regular mr-1 w-4 text-sm mb-2" />
+              <a href="" className="text-black font-regular text-sm mb-2">Terms and Conditions</a>
+            </div>
+            <button type="submit" onClick={handleSubmit} className={`my-2 w-78 ${style} rounded-sm  font-bolds py-2`}>Complete</button>
+            <button onClick={handleB} className='bg-secondary block transition duration-300 hover:bg-primary hover:text-blue-300 text-white px-4 py-1.5 rounded-sm cursor-pointer font-bolds '>Back</button>
+            </div>
+            <p className={`absolute ${show} bg-white text-red-600 top-[7vh] text-shadow-2xs font-light rounded-sm px-1`}>{p}</p>
+          </form>
         </div>
-        <div className={`submit ${disp[1]}`}>
-              <Image src={img} width={124} className='mx-auto rounded-full max-w-31 max-h-31' height={124} alt='Profile'></Image>
-            <div className="each-part mt-1 flex items-center justify-between w-78">
-            <p className="text-primary font-regular text-sm">Upload profile picture</p>
-            <input type="file" onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
-              const val = e.target.files?.[0]
-              if(val){
-                const prevData = {...data}
-                prevData.Profile = val
-                setData(prevData)
-                const url = URL.createObjectURL(val)
-                setImg(url)
-              }
-            }} accept='image/*' className="text-secondary font-regular w-[160px] text-sm" />
-        </div>
-        <div className="each-part mt-1 flex items-center justify-start w-78">
-          <input type="checkbox" onClick={()=>setCheck(!check)} className="font-regular mr-1 w-4 text-sm mb-2" />
-          <a href="" className="text-black font-regular text-sm mb-2">Terms and Conditions</a>
-        </div>
-        <button type="submit" onClick={handleSubmit} className={`my-2 w-78 ${style} rounded-sm  font-bolds py-2`}>Complete</button>
-        <button onClick={handleB} className='bg-secondary block transition duration-300 hover:bg-primary hover:text-blue-300 text-white px-4 py-1.5 rounded-sm cursor-pointer font-bolds '>Back</button>
-        </div>
-        <p className={`absolute ${show} bg-white text-red-600 top-[7vh] text-shadow-2xs font-light rounded-sm px-1`}>{p}</p>
-      </form>
     </div>
   </>
   )
